8726d4c92cca8e9177e137470947e84343a3ff9a
LukasDano <lukas.danowski@mail.de>
2025-12-08 10:44:27 +0100

    #19 sorting replaced to german

--- frontend/src/components/content/tables/leaderBoard/LeaderBoard.tsx
+++ frontend/src/components/content/tables/leaderBoard/LeaderBoard.tsx
@@ -9,7 +9,7 @@ import { type AcceptedFilterOptions, FilterModal } from '../../../modal/FilterMo
 import { TableHeadline } from '../TableHeadline.tsx';
 import { LeaderBoardRow } from './LeaderBoardRow.tsx';
 
-export type SortField = 'distance' | 'days';
+export type SortField = 'Strecke' | 'Lauftage';
 
 export const LeaderBoard = () => {
     const { rankList } = useContext(AppContext);
@@ -20,15 +20,15 @@ export const LeaderBoard = () => {
     const [filteredList, setFilteredList] = useState<Rank[]>(rankList);
 
     const [sortOrder, setSortOrder] = useState<SortOrder>('desc');
-    const [sortField, setSortField] = useState<SortField>('distance');
+    const [sortField, setSortField] = useState<SortField>('Strecke');
 
     useEffect(() => {
         const newList = [...rankList]
             .filter((rank) => matchesFilters(rank, filterOptions))
             .sort((rankA, rankB) => {
-                if (sortField === 'distance') {
+                if (sortField === 'Strecke') {
                     return sortOrder === 'asc' ? rankA.total_km - rankB.total_km : rankB.total_km - rankA.total_km;
-                } else if (sortField === 'days') {
+                } else if (sortField === 'Lauftage') {
                     return sortOrder === 'asc' ? rankA.lauf_tage - rankB.lauf_tage : rankB.lauf_tage - rankA.lauf_tage;
                 } else {
                     return sortOrder === 'asc' ? rankA.total_km - rankB.total_km : rankB.total_km - rankA.total_km;
@@ -53,7 +53,7 @@ export const LeaderBoard = () => {
                 onFilterClick={() => setFilterModalOpen(true)}
                 sortOrder={sortOrder}
                 onSortOrderChange={(val) => setSortOrder(val)}
-                sortFields={['distance', 'days']}
+                sortFields={['Strecke', 'Lauftage']}
                 currentSortField={sortField}
                 onSortFieldChange={(val) => setSortField(val)}
             />
